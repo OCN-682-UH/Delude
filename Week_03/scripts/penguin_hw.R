53267a88edfbf1d80f09436b256514b0c29e8d12
mdelude-source <mdelude@hawaii.edu>
2026-09-12 19:25:24 -1000

    New Week_03 folder

---
+++ Week_03/scripts/penguin_hw.R
@@ -0,0 +1,32 @@
+##This script is for analyzing the Palmer Penguin data for Week_03's classwork/homework
+
+##Necessary Packages (skip if already installed)
+#install.packages("palmerpenguins")
+
+##Libraries
+library(palmerpenguins)
+library(tidyverse)
+glimpse(penguins)
+
+##Functions
+  #NA for now
+
+##Data
+  #Already loaded in as "penguins"
+
+##Code
+  ##The old code from lecture 5, for reference.
+
+#ggplot(data=penguins,
+#       mapping = aes( x = bill_depth_mm,
+#                      y = bill_length_mm,
+#                      color = species)) +
+#  geom_point() +
+#    labs(title = "Bill depth and length",
+#         subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+#         x = "Bill depth (mm)", y = "Bill length(mm)",
+#         color = "Species",
+#         caption = "Source: Palmer Station LTER / palmerpenguins package") +
+#  scale_color_viridis_d()
+
+  ##Homework code starts here
